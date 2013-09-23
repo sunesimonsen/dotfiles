1717a843306072e30c03da9320fb78008874d15e
Sune Simonsen <sune@we-knowhow.dk>
2013-09-23 22:17:09 +0200

    Tweaking emacs

--- evil.d/custom.el
+++ evil.d/custom.el
@@ -1,12 +1,13 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(backup-directory-alist (quote ((".*" . "~/.evil.backups"))))
  '(case-replace nil)
  '(dabbrev-case-distinction nil)
  '(dabbrev-case-replace nil)
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(global-auto-revert-mode t)
  '(hl-paren-colors (quote ("green" "yellow" "magenta" "orange")))
  '(ido-auto-merge-delay-time 100)
@@ -29,8 +30,8 @@
  '(yas-prompt-functions (quote (yas-ido-prompt yas-x-prompt yas-dropdown-prompt yas-completing-prompt yas-ido-prompt yas-no-prompt)))
  '(yas/prompt-functions (quote (yas/ido-prompt yas/x-prompt yas/dropdown-prompt yas/completing-prompt yas/no-prompt))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
