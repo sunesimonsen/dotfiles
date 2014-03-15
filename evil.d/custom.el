5c0192972a59af1248b7e0d7b030428597d19f12
Sune Simonsen <sune@we-knowhow.dk>
2014-03-15 16:41:12 +0100

    Use evil search

--- evil.d/custom.el
+++ evil.d/custom.el
@@ -9,7 +9,7 @@
  '(dabbrev-case-distinction nil)
  '(dabbrev-case-replace nil)
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(evil-search-module (quote isearch))
+ '(evil-search-module (quote evil-search))
  '(global-auto-revert-mode t)
  '(goto-alternate-git-file-patterns (quote (("\\.spec\\.js$" . ".js") ("\\.h$" . ".c") ("Test\\.java$" . ".java") ("\\.ko$" . ".less") ("\\.less$" . ".ko"))))
  '(hl-paren-colors (quote ("green" "yellow" "magenta" "orange")))
@@ -37,4 +37,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(error ((t (:underline t)))))
+ '(error ((t (:underline t))))
+ '(lazy-highlight ((t (:background "gray13")))))
