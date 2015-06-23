3d4864fb4858248b179ac35e201eb54fd09c0959
Sune Simonsen <sune@we-knowhow.dk>
2015-06-23 07:53:22 +0200

    Color status string

--- i3/status.sh
+++ i3/status.sh
@@ -9,8 +9,16 @@ do
     TIMECLOCK='['
     if [ -e /tmp/emacs$USERID/server ]
     then
+        STATUS=`emacsclient --eval "(timeclock-status-string)" | sed 's/"//g'`
+        COLOR="#00FF00"
+        if [[ $STATUS == *"OUT"* ]]
+        then
+            COLOR="#FF0000"
+        fi
         TIMECLOCK+='{"name":"timeclock","instance":"one","full_text":"'
-        TIMECLOCK+=`emacsclient --eval "(timeclock-status-string)" | sed 's/"//g'`
+        TIMECLOCK+=$STATUS
+        TIMECLOCK+='","color":"'
+        TIMECLOCK+=$COLOR
         TIMECLOCK+='"},'
     fi
     echo "${line/[/[$TIMECLOCK}" || exit 1
